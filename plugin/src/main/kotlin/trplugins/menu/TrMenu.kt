713d5ec2f6d3a083db7e61d34cd0d29e6e2b85d3
shuiqing2046 <28628358+shuiqing2046@users.noreply.github.com>
2023-09-07 13:15:45 +0800

    [3.1.27] Update TabooLib 6.0.12-20 > 1.20.1 Support
    Update TabooLib fix some bugs

--- plugin/src/main/kotlin/trplugins/menu/TrMenu.kt
+++ plugin/src/main/kotlin/trplugins/menu/TrMenu.kt
@@ -29,7 +29,7 @@ import trplugins.menu.module.internal.script.evalScript
  */
 object TrMenu : Plugin() {
 
-    @Config("settings.yml",true, autoReload = true)
+    @Config("settings.yml", true.toString(), autoReload = true)
     lateinit var SETTINGS: Configuration
         private set
 
