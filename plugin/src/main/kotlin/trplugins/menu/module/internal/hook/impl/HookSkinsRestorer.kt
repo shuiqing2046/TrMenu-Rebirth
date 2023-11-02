9be63df811c10d1f3a2bc8c9a4bd40af56c93cf0
磁铁开发部（MDD） <128666602+MSCMDD@users.noreply.github.com>
2023-11-02 23:04:51 +0800

    Update HookSkinsRestorer.kt

--- plugin/src/main/kotlin/trplugins/menu/module/internal/hook/impl/HookSkinsRestorer.kt
+++ plugin/src/main/kotlin/trplugins/menu/module/internal/hook/impl/HookSkinsRestorer.kt
@@ -1,17 +1,22 @@
 package trplugins.menu.module.internal.hook.impl
 
-import net.skinsrestorer.api.SkinsRestorerAPI
+import net.skinsrestorer.api.SkinsRestorer
+import net.skinsrestorer.api.exception.DataRequestException;
+import net.skinsrestorer.api.exception.MineSkinException;
+import net.skinsrestorer.api.SkinsRestorerProvider
+import net.skinsrestorer.api.storage.PlayerStorage
+import net.skinsrestorer.api.storage.SkinStorage
 import trplugins.menu.module.internal.hook.HookAbstract
 
 /**
- * @author Arasple
- * @date 2021/1/27 14:12
+ * @author AoMe
+ * @date 2023/11/02 14:12
  */
 class HookSkinsRestorer : HookAbstract() {
 
-    private val skinsRestorerAPI: SkinsRestorerAPI? =
+    private val SkinsRestorer: SkinsRestorerAPI? =
         if (isHooked) {
-            SkinsRestorerAPI.getApi()
+            SkinsRestorerAPI.getPlayerStorage()
         } else {
             null
         }
@@ -22,11 +27,11 @@ class HookSkinsRestorer : HookAbstract() {
 
     fun getPlayerSkinTexture(name: String): String? {
         skinsRestorerAPI?.let {
-            if (it.getSkinData(name) == null) {
+            if (it.getSkinForPlayer(name) == null) {
                 return null
             }
 
-            val skinData = it.getSkinData(name)
+            val skinData = it.getSkinForPlayer(name)
             if (skinData != null) {
                 return skinData.value
             }
